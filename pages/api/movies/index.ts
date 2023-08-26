5f2b984dce1e2255cacfbfe24cd49f2dafaf7c7f
markande98 <gauravbpit1998@gmail.com>
2023-08-26 15:13:05 +0530

    movie api & related hook

---
+++ pages/api/movies/index.ts
@@ -0,0 +1,24 @@
+import serverAuth from "@/lib/serverAuth";
+import { NextApiRequest, NextApiResponse } from "next";
+
+import prisma from '@/lib/prismadb';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+    if(req.method !== 'GET'){
+        return res.status(405).end();
+    }
+
+    try{
+        await serverAuth(req, res);
+
+        const movies = await prisma.movie.findMany();
+
+        return res.status(200).json(movies);
+    } catch(error){
+        console.log(error);
+        return res.status(400).end();
+    }
+}
